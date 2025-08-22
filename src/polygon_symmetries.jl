805e2f6aa8780fa65f2dde1f4f4da76ee7328737
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2025-08-22 17:25:19 +0100

    polygon_symmetries: fix for rotational-only symmetric polygons

--- src/polygon_symmetries.jl
+++ src/polygon_symmetries.jl
@@ -3,10 +3,7 @@ export PolygonSymmetries
 using StaticArrays
 
 "Reflect `point ∈ C` about an axis in `C`"
-function reflect(axis, point)
-    normalised_axis = axis / abs(axis)
-    normalised_axis * conj(normalised_axis' * point)
-end
+reflect(axis, point) = axis * conj(axis' * point) / abs2(axis)
 
 """Constructive polygon symmetry information
 
@@ -18,15 +15,15 @@ In order to relate the total number of vertices to independent free
 parameters, we need to know how many vertices are fixed to be on symmetry
 axes.
 
-:rotational_symmetry: Number of discrete rotation steps until a full
+:rotational_order: Number of discrete rotation steps until a full
     2π rotation is achieved.
 :has_mirror_symmetry: If the polygon has a mirror symmetry. If `true`, there are
-    `rotational_symmetry` mirror symmetries in total.
+    `rotational_order` mirror symmetries in total.
 :points_on_axes: Number of vertices ∈ {0,1,2} lying on the principal mirror
     symmetry axes.
 """
 struct PolygonSymmetries
-    rotational_symmetry::Int64
+    rotational_order::Int64
     has_mirror_symmetry::Bool
     points_on_axes::Int64
 end
@@ -44,31 +41,31 @@ function PolygonSymmetries(poly::Polygon{N,W,B,M}) where {N,W,B,M}
         end
     end
 
+    # go through all possible mirror symmetries
+    midpoints = (poly.w .+ circshift(poly.w, 1)) / 2
+    potential_symmetry_points = sort([poly.w; midpoints], by = angle)
+
     function is_symmetry_axis(axis)
         poly_reflect = reverse(reflect.(axis, poly.w))
         # check permutations for congruence
         any(all(poly_reflect .≈ circshift(poly.w, i)) for i = 0:N-1)
     end
 
-    midpoints = (poly.w .+ circshift(poly.w, 1)) / 2
-    potential_symmetry_points = sort([poly.w; midpoints], by = angle)
     idx_1 = findfirst(is_symmetry_axis, potential_symmetry_points)
     has_mirror_symmetry = !isnothing(idx_1)
-    axis_1 = potential_symmetry_points[idx_1]
-
     points_on_axes = 0
     if has_mirror_symmetry
-        points_on_axes += count(≈(axis_1), poly.w)
-    end
-    if rot_order > 1
-        # there must be a second mirror symmetry
+        axis = potential_symmetry_points[idx_1]
+        points_on_axes += count(==(axis), poly.w)
+
+        # search for second mirror symmetry
         idx_2 = findnext(is_symmetry_axis, potential_symmetry_points, idx_1+1)
-        @assert !isnothing(idx_2) "Expected a second mirror symmetry from rotation"
-        axis_2 = potential_symmetry_points[idx_2]
-        points_on_axes += count(≈(axis_2), poly.w)
-    else
-        # count all points on the single mirror symmetry axis
-        points_on_axes += count(≈(-axis_1), poly.w)
+        if isnothing(idx_2)
+            # count all points on the single mirror symmetry axis
+            points_on_axes += count(==(-axis), poly.w)
+        else
+            points_on_axes += count(==(potential_symmetry_points[idx_2]), poly.w)
+        end
     end
 
     PolygonSymmetries(rot_order, has_mirror_symmetry, points_on_axes)
