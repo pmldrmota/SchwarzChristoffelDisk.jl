a63cd90691fda41b33b623a88cbf5a6cc5069404
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2025-08-29 15:00:42 +0100

    polygon: rewrite symmetry classification
    trick 1: use length and turn angles as identifiers (as complex numbers)
    trick 2: replace infinite edge lengths with unique finite length
    missing: calculation of axis angles in real space
    See Eades 1988

--- src/polygon.jl
+++ src/polygon.jl
@@ -38,75 +38,36 @@ end
 :param β: left-turn angles at the nodes of the polygon
 :returns: instance of a subtype of AbstractSymmetry
 """
-function classify_symmetry(w::SVector{N}, β::SVector{N}) where {N}
-    # precompute circshifts
-    circshifts_w = SMatrix{N}((w[mod1(i - k, N)] for k ∈ 1:N for i ∈ 1:N)...)
-    circshifts_β = SMatrix{N}((β[mod1(i - k, N)] for k ∈ 1:N for i ∈ 1:N)...)
+function classify_symmetry(w::SVector{N}, β::SVector{N}, ℓ::SVector{N}) where {N}
+    # want to preserve angle information on infinite vertices
+    # replace all infinite edges with a unique finite length
+    my_inf = sum(filter(!isinf, ℓ))  # ∉ ℓ
+    ℓ = SVector(replace(x -> isinf(x) ? my_inf : x, ℓ))
+    L = MVector(ℓ .* cispi.(β))
+    M = MVector(ℓ .* cispi.(circshift(β, -1)) |> reverse)
 
-    isinf_or_nan(x) = isinf(x) || isnan(x)
-    is_approx_equal(w₁, β₁, w₂, β₂) =
-        isinf_or_nan(w₁) && isinf_or_nan(w₂) && mod(β₁, 2) == mod(β₂, 2) || w₁ ≈ w₂
+    rot_equivalent(A, B, rot_a) = all(B .≈ circshift(A, rot_a))
 
-    is_congruent(nodes, turn_angles) = any(
-        all(is_approx_equal.(nodes, turn_angles, sw, sβ)) for
-        (sw, sβ) ∈ zip(eachcol(circshifts_w), eachcol(circshifts_β))
-    )
+    rot_order = 1 + count(k -> rot_equivalent(L, L, k), 1:N-1)
+    reflections = [m for m ∈ 0:N-1 if rot_equivalent(L, M, m)]
 
-    is_rotational_symmetry(k) = is_congruent(cispi(2 // k) * w, β)
-    # still not correct because equal β at opposite infinities does not mean
-    # the lines coming from ∞ are mirror images of each other (there can be an offset).
-    # this wouldn't be captured by the finite endpoints if they themselves are on the axis.
-    is_mirror_symmetry(α) = is_congruent(reverse(@. α * conj(α' * w) / abs2(α)), reverse(β))
-
-    # classify rotational symmetry
-    rotational_order = N + 1 - something(findfirst(is_rotational_symmetry, N:-1:2), N)
-    has_rotation = rotational_order > 1
-
-    # candidate mirror symmetry axes: finite points and edge normals
-    # if w[i] is its own mirror image, then the axis would be along w[i]
-    finite_w = filter(!isinf, w)
-    # if w[1] has a mirror image, w[idx], then the axis would be normal to w[1] - w[idx]
-    edge_normals = (finite_w[2:end] .- finite_w[1]) * cispi(1 // 2)
-    # filter out those that are identical under 180° rotation and eliminate duplicates
-    # we can't use `unique()` because we have numerical precision.
-    axes = []
-    for ax ∈ [finite_w; edge_normals]
-        ϕ = angle(ax)
-        ax_upper = (ϕ ≥ 0 ? ax : -ax) / abs(ax)
-        if isnothing(findfirst(≈(ax_upper), axes))
-            push!(axes, ax_upper)
-        end
-    end
-    axes = sort(axes, by = angle)
-
-    # find first symmetry axis, if any
-    idx₁ = findfirst(is_mirror_symmetry, axes)
-    has_mirror = !isnothing(idx₁)
-
-    on_positive_axis(ax) = p -> ax * conj(p) ≈ abs(p)
-    points_on_axes = 0
-
-    if has_mirror
-        axis₁ = axes[idx₁]
-        points_on_axes += count(on_positive_axis(axis₁), finite_w)
-        # look for another axis
-        idx₂ = findnext(is_mirror_symmetry, axes, idx₁ + 1)
-        # if no other found, then we need to use the negative part of axis₁
-        axis₂ = isnothing(idx₂) ? -axis₁ : axes[idx₂]
-        points_on_axes += count(on_positive_axis(axis₂), finite_w)
-    end
-
-    # are polygons whose finite vertices are symmetric also symmetric in the
-    # prevertices even if the overall polygon doesn't have that symmetry?
+    has_rotation = rot_order > 1
+    has_mirror = length(reflections) > 0
     if has_rotation && has_mirror
-        α = angle(axes[idx₁])
-        DihedralSymmetry{rotational_order,points_on_axes,typeof(α)}(α)
+        # With 2 adjacent mirror axes, we can just count how many of them
+        # include a vertex.
+        points_on_axes = count(r -> iseven(r + N), reflections[1:2])
+        α = NaN
+        DihedralSymmetry{rot_order,points_on_axes,typeof(α)}(α)
     elseif !has_rotation && has_mirror
-        α = angle(axes[idx₁])
-        # issue: isodd(N) && (w : isinf(w)) is on axis => points_on_axes wrong
+        # With 1 mirror axis only, a polygon with an odd number of vertices
+        # must have exactly 1 vertex on the axis. Otherwise, there are either
+        # 0 or 2 vertices on the axis.
+        points_on_axes = isodd(N) ? 1 : (iseven(reflections[1] + N) ? 2 : 0)
+        α = NaN
         BilateralSymmetry{points_on_axes,typeof(α)}(α)
     elseif has_rotation && !has_mirror
-        CyclicSymmetry{rotational_order}()
+        CyclicSymmetry{rot_order}()
     else
         NoSymmetry()
     end
@@ -119,7 +80,7 @@ struct Polygon{N,S<:AbstractSymmetry,W,F}
     ℓ::SVector{N,F}  # length of edges [i,i+1]
 
     function Polygon(w::SVector{N,W}, β::SVector{N,F}, ℓ::SVector{N,F}) where {N,W,F}
-        sym = classify_symmetry(w, β)
+        sym = classify_symmetry(w, β, ℓ)
         new{N,typeof(sym),W,F}(w, sym, β, ℓ)
     end
 end
