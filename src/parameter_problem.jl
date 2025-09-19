b47c217a1f802bbf503d47f7c40558ff4e6d8872
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2025-09-19 20:44:09 +0100

    parameter problem: rewrite without assumptions on indexing

--- src/parameter_problem.jl
+++ src/parameter_problem.jl
@@ -15,14 +15,14 @@ end
 
 """Fix the scaling constant of the Schwarz-Christoffel transformation and set the singularities
 """
-function sc_fix!(f, θ, wN)
+function sc_fix!(f, θ, kN, wN)
     f.z .= cispi.(θ)
-    @assert !any(isnan.(f.z)) "sc_fix! got NaN output $(f.z) from $θ"
+    @assert !any(isnan.(f.z)) "sc_fix! got NaN output $(f.z) from $θ."
 
     # evaluate image vertices with unit constant
     f.c = 1
     # fix scaling constant such that wN is correct
-    f.c = wN / sc_trafo(f, f.z[end])
+    f.c = wN / sc_trafo(f, f.z[kN])
 end
 
 # dispatch free parameters based on typed symmetry
@@ -49,41 +49,90 @@ prevertices(v::MVector{V,T}, ::DihedralSymmetry{R,1}) where {V,T,R} =
 prevertices(v::MVector{V,T}, ::DihedralSymmetry{R,2}) where {V,T,R} =
     prevertices(MVector{2 * V + 2,T}(v..., 0, -v[end:-1:1]..., 0), CyclicSymmetry{R}())
 
-function sc_parameter_problem(poly::Polygon{N}) where {N}
-    x₀ = free_params(poly)
-    f = SchwarzChristoffel(prevertices(x₀, poly.s), poly.β)
+struct ProblemIndices{X,L,T}
+    kN::T
+    k_fix::SVector{X,T}
+    k_len::SVector{L,T}
 
-    k_inf = findall(isinf, poly.w)
-    # vertex positions
-    k_fix = tuple(1, (k_inf[2:end] .- 1)...)
-    # n-2m-1 side lengths
-    k_fin = tuple(findall(isfinite, poly.ℓ)[1:(N-2*length(k_fix)-1)]...)
+    function ProblemIndices(poly::Polygon{N}) where {N}
+        idx₁ = first_independent_vertex(poly)
+        n = num_independent_vertices(poly)
+        range = SVector{n}(mod1(idx₁ + i, N) for i ∈ 0:(n-1))
 
-    function cost_function!(F, x)
-        sc_fix!(f, prevertices(x, poly.s), poly.w[end])
+        # last one reaches outside the symmetry-generating set
+        k_len = filter(k -> isfinite(poly.ℓ[k]), range)
+        # use the first one as kN
+        kN = popfirst!(k_len)
+        # the connected vertex to fix the rotation and translation
+        k1 = mod1(kN + 1, N)
 
-        if !isnothing(F)
-            # fix one vertex per component - 2 real conditions each
-            @inbounds for (i, k) ∈ enumerate(k_fix)
-                dk = poly.w[k] - sc_trafo(f, f.z[k])
-                F[2i-1] = real(dk)
-                F[2i] = imag(dk)
+        # we need one fixed vertex per segment between infinities
+        k_fix = Int64[k1]
+        # add the one before each infinity / last in range
+        k_d = range[1]
+        skip_current = (k_d == k1)
+        for k ∈ range[2:end]
+            if k == k1
+                skip_current = true
             end
-            # fix n-2m-1 side lengths - 1 real condition each
-            m = length(k_fix)
-            @inbounds for (j, k) ∈ enumerate(k_fin)
-                F[2m+j] = abs(sc_segment(f, k)) - poly.ℓ[k]
+            if isinf(poly.w[k]) || k == range[end]
+                if !skip_current
+                    push!(k_fix, k_d)
+                end
+                skip_current = false
             end
+            k_d = k
         end
+
+        # need indices of finite ℓ
+        num_x = length(k_fix)
+        k_fix = SVector{num_x}(k_fix)
+        num_ℓ = length(free_params(poly)) - 2 * num_x
+        k_len = SVector{num_ℓ}(k_len[1:num_ℓ])
+        new{num_x,num_ℓ,Int64}(kN, k_fix, k_len)
     end
+end
 
-    # solve
-    sol = nlsolve(cost_function!, x₀)
-    # apply solution
-    sc_fix!(f, prevertices(sol.zero, poly.s), poly.w[end])
+function cost_function!(F, x, f, poly, idxs::ProblemIndices{X}) where {X}
+    sc_fix!(f, prevertices(x, poly.s), idxs.kN, poly.w[idxs.kN])
 
+    # fix one vertex per component - 2 real conditions each
+    @inbounds for (i, k) ∈ enumerate(idxs.k_fix)
+        dk = poly.w[k] - sc_trafo(f, f.z[k])
+        F[2i-1] = real(dk)
+        F[2i] = imag(dk)
+    end
+    # fix n-2m-1 side lengths - 1 real condition each
+    @inbounds for (j, k) ∈ enumerate(idxs.k_len)
+        F[2X+j] = abs(sc_segment(f, k)) - poly.ℓ[k]
+    end
+end
+
+function sc_parameter_problem(poly::Polygon{N}) where {N}
+    # todo: order of prevertices is inconsistent
+    (sol, f) = if num_independent_vertices(poly) == 1
+        # trivial case does not require NLsolve
+        k = findfirst(isfinite, poly.w)
+        θ₀ = prevertices(free_params(poly), poly.s)
+        f = SchwarzChristoffel(θ₀, poly.β)
+        sc_fix!(f, θ₀, k, poly.w[k])
+        (nothing, f)
+    else
+        (p, idxs) = try
+            (poly, ProblemIndices(poly))
+        catch
+            p = Polygon(poly.w, NoSymmetry(), poly.β, poly.ℓ)
+            (p, ProblemIndices(p))
+        end
+        x₀ = free_params(p)
+        f = SchwarzChristoffel(prevertices(x₀, p.s), p.β)
+        # solve
+        sol = nlsolve((F, x) -> cost_function!(F, x, f, p, idxs), x₀)
+        # apply solution
+        sc_fix!(f, prevertices(sol.zero, p.s), idxs.kN, p.w[idxs.kN])
+        (sol, f)
+    end
     # test
     !sc_test_ok(f, poly.w) && @warn "parameter_problem failed"
-
     (sol, f)
 end
