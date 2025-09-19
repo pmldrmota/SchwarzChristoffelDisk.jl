14e0d2e16a33d1cca1f3731ef7fd6bb16eb57e57
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2025-09-19 20:32:40 +0100

    polygon: add num_independent_vertices and first_independent_vertex methods

--- src/polygon.jl
+++ src/polygon.jl
@@ -1,4 +1,4 @@
-export Polygon
+export Polygon, first_independent_vertex, num_independent_vertices
 
 using StaticArrays
 
@@ -144,4 +144,64 @@ function Polygon(w::SVector{N,W}, β::SVector{N,F}) where {N,W,F}
     ℓ = [abs(w[mod1(i + 1, N)] - w[i]) for i ∈ eachindex(w)]
     # todo: check supplied angles β for inconsistencies with supplied vertices w
     Polygon(SVector{N,W}(w), SVector{N,F}(β), SVector{N,F}(ℓ))
+end
+
+num_independent_vertices(::Polygon{N,NoSymmetry}) where {N} = N
+num_independent_vertices(::Polygon{N,CyclicSymmetry{R}}) where {N,R} = N ÷ R
+num_independent_vertices(::Polygon{N,<:BilateralSymmetry{P}}) where {N,P} = P + (N - P) ÷ 2
+num_independent_vertices(::Polygon{N,<:DihedralSymmetry{R,P}}) where {N,R,P} =
+    P + (N ÷ R - P) ÷ 2
+
+"Symmetry start index"
+first_independent_vertex(::Polygon{<:Any,NoSymmetry}) = 1
+first_independent_vertex(::Polygon{<:Any,<:CyclicSymmetry}) = 1
+
+function is_left(axis, point)
+    u = imag(conj(axis) * point)
+    u < 0 && abs(u) > abs(axis) * √eps()
+end
+
+""" Bilateral symmetry start index
+
+For P=1 and P=2 we search for patterns
+     l.r
+     l∞r
+    l∞.∞r
+"""
+first_independent_vertex(poly::Polygon{N,<:BilateralSymmetry}) where {N} =
+    findfirst(i -> begin
+        w = poly.w[i]
+        if isinf(w)
+            w₋ = poly.w[mod1(i - 1, N)]
+            w₊ = poly.w[mod1(i + 1, N)]
+            is_left(poly.s.axis, w₋) != is_left(poly.s.axis, w₊)
+        else
+            v = poly.s.axis / abs(poly.s.axis) * abs(w)
+            v ≈ w || v ≈ -w
+        end
+    end, 1:N)
+
+""" Bilateral symmetry start index
+
+For P=0 we search for the finite vertices whose edge is divided by the axis.
+pattern: lr
+"""
+first_independent_vertex(poly::Polygon{N,<:BilateralSymmetry{0}}) where {N} = findfirst(
+    i -> begin
+        w₋ = poly.w[mod1(i - 1, N)]
+        w = poly.w[i]
+        isfinite(w₋) &&
+            isfinite(w) &&
+            (is_left(poly.s.axis, w₋) != is_left(poly.s.axis, w))
+    end,
+    1:N,
+)
+
+""" Dihedral symmetry start index
+
+see BilateralSymmetry
+"""
+function first_independent_vertex(poly::Polygon{<:Any,<:DihedralSymmetry{<:Any,P}}) where {P}
+    p = Polygon(poly.w, BilateralSymmetry{P}(poly.s.axis), poly.β, poly.ℓ)
+    first_independent_vertex(p)
 end
